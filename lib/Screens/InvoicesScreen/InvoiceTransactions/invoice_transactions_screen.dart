b2c6f8e96536987e5eeec960f76f7ee6bd8fd76c
i15tech <i15tech.global@gmail.com>
2024-10-29 15:22:58 +0530

    Quickcash
    -Invoices Screens
      - Categories(Invoice) Screen Design Done
         - Search
         - Add Categories
         - Show All Categories
         - Edit Categories
         - Delete Categories
      - Product(Invoice) Screen Design Done
         - Search
         - Add Product
         - Show All Product
         - Edit Product
         - Delete Product
         - View Product
      - Invoices(Invoice) Screen Design Done
         - Show All Invoices List
         - Invoice URl
         - Reminder
         - Start Recurring
      - Manual Payment(Invoice) Screen Design Done
         - Search
         - Add Manual Payment
         - Show all payment List
         - View Payment
         - Delete Payment
      - Invoice Transaction(Invoice) Screen Design Done

--- lib/Screens/InvoicesScreen/InvoiceTransactions/invoice_transactions_screen.dart
+++ lib/Screens/InvoicesScreen/InvoiceTransactions/invoice_transactions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quickcash/components/background.dart';
+import 'package:quickcash/constants.dart';
 
 class InvoiceTransactionsScreen extends StatefulWidget {
   const InvoiceTransactionsScreen({super.key});
@@ -10,20 +11,139 @@ class InvoiceTransactionsScreen extends StatefulWidget {
 
 class _InvoiceTransactionsScreenState extends State<InvoiceTransactionsScreen> {
 
+  final List<Map<String, String>> invoiceTransactionList = [
+    {
+      'invoiceNumber': 'ITIOXX9X9O18958',
+      'paymentDate': '2024-10-20',
+      'total': '1008',
+      'paidAmount': '10555508',
+      'transactionType': 'Manual',
+    },
+    {
+      'invoiceNumber': 'ITIOXX9X9O189566571',
+      'paymentDate': '2024-10-23',
+      'total': '10708',
+      'paidAmount': '100558',
+      'transactionType': 'Manual',
+    },
+    // You can add more card entries here
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Background(
-      child: SafeArea(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+            "Invoice Transaction",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(defaultPadding),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // You can add more widgets below this Row if needed
-            Expanded(
-              child: Center(
-                child:
-                Text('Invoice Transaction Screen'), // Placeholder for main content
-              ),
+
+            const SizedBox(height: largePadding,),
+
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: invoiceTransactionList.length,
+              itemBuilder: (context, index) {
+                final card = invoiceTransactionList[index];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: defaultPadding), // Add spacing
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(defaultPadding),
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 8,
+                          spreadRadius: 1,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Invoice Number:', style: TextStyle(color: Colors.white, fontSize: 16),),
+                            Text('${card['invoiceNumber']}', style: const TextStyle(color: Colors.white, fontSize: 16),),
+                          ],
+                        ),
+
+                        const SizedBox(height: smallPadding,),
+                        const Divider(color: kPrimaryLightColor,),
+                        const SizedBox(height: smallPadding,),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Payment Date:',style: TextStyle(color: Colors.white, fontSize: 16),),
+                            Text('${card['paymentDate']}',style: const TextStyle(color: Colors.white, fontSize: 16),),
+                          ],
+                        ),
+
+                        const SizedBox(height: smallPadding,),
+                        const Divider(color: kPrimaryLightColor,),
+                        const SizedBox(height: smallPadding,),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Total:',style: TextStyle(color: Colors.white, fontSize: 16),),
+                            Text('${card['total']}',style: const TextStyle(color: Colors.white, fontSize: 16),),
+                          ],
+                        ),
+
+                        const SizedBox(height: smallPadding,),
+                        const Divider(color: kPrimaryLightColor,),
+                        const SizedBox(height: smallPadding,),
+
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Paid Amount:',style: TextStyle(color: Colors.white, fontSize: 16),),
+                            Text('${card['paidAmount']}',style: const TextStyle(color: Colors.white, fontSize: 16),),
+                          ],
+                        ),
+
+                        const SizedBox(height: smallPadding,),
+                        const Divider(color: kPrimaryLightColor,),
+                        const SizedBox(height: smallPadding,),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Transaction Type:',style: TextStyle(color: Colors.white, fontSize: 16),),
+                            Text('${card['transactionType']}',style: const TextStyle(color: Colors.white, fontSize: 16),),
+                          ],
+                        ),
+
+
+
+                        const SizedBox(height: smallPadding,),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
+
           ],
+        ),
         ),
       ),
     );
