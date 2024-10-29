ec7c91613e6bed996b4474145e79d750cf54ad2c
i15tech <i15tech.global@gmail.com>
2024-10-29 11:28:49 +0530

    Quickcash
    -Invoices Screens
      - Invoice Dashboard Screen Done
         - Quotes Dashboard Screen Design Done
         - Add Invoice Screen Design Done
         - Add Quote Screen Design Done
      - Client(Invoice) Screen Design Done
         - Add Clients Screen Design Done
         - Edit Clients Screen Design Done
         - View Clients Screen Design Done
         - Client Home Screen Design Done
      - Categories(Invoice) Screen Design Done
         - Search
         - Add Categories
         - Show All Categories
         - Edit Categories
         - Delete Categories
      - Product(Invoice) Screen Design Done
         - Search
         - Add Product
         - Show All Product
         - Edit Product
         - Delete Product
         - View Product

--- lib/Screens/InvoicesScreen/ProductsScreen/products_screen.dart
+++ lib/Screens/InvoicesScreen/ProductsScreen/products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quickcash/components/background.dart';
+import 'package:quickcash/Screens/InvoicesScreen/ProductsScreen/AddProductScreen/add_product_screen.dart';
+import 'package:quickcash/constants.dart';
 
 class ProductsScreen extends StatefulWidget {
   const ProductsScreen({super.key});
@@ -9,24 +10,236 @@ class ProductsScreen extends StatefulWidget {
 }
 
 class _ProductsScreenState extends State<ProductsScreen> {
+  final List<Map<String, String>> clientsList = [
+    {
+      'createdDate': '2024-10-20',
+      'productName': 'Test Product',
+      'category': 'Business',
+      'price': '108',
+    },
+    // You can add more card entries here
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return const Background(
-      child: SafeArea(
-        child: Column(
-          children: [
-            // You can add more widgets below this Row if needed
-            Expanded(
-              child: Center(
-                child:
-                Text('Products Screen'), // Placeholder for main content
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          "Products",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(padding: const EdgeInsets.all(defaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      cursorColor: kPrimaryColor,
+                      onSaved: (value) {
+
+                      },
+
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultPadding),
+                          borderSide: const BorderSide(),
+                        ),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        hintText: "Search",
+                        hintStyle: const TextStyle(color: kHintColor),
+                        prefixIcon: const Padding(
+                          padding: EdgeInsets.all(defaultPadding),
+                          child: Icon(Icons.search,color: kHintColor,),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(width: defaultPadding,),
+
+                  FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const AddProductScreen()),
+                      );
+                    },
+                    child: const Icon(Icons.add, color: kPrimaryColor,),
+                  ),
+                ],
               ),
-            ),
-          ],
+
+              const SizedBox(height: largePadding,),
+
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: clientsList.length,
+                itemBuilder: (context, index) {
+                  final card = clientsList[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: defaultPadding),
+                    // Add spacing
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(defaultPadding),
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 8,
+                            spreadRadius: 1,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Created Date:', style: TextStyle(
+                                  color: Colors.white, fontSize: 16),),
+                              Text('${card['createdDate']}',
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 16),),
+                            ],
+                          ),
+
+                          const SizedBox(height: smallPadding,),
+                          const Divider(color: kPrimaryLightColor,),
+                          const SizedBox(height: smallPadding,),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Product Name:', style: TextStyle(
+                                  color: Colors.white, fontSize: 16),),
+                              Text('${card['productName']}',
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 16),),
+                            ],
+                          ),
+
+                          const SizedBox(height: smallPadding,),
+                          const Divider(color: kPrimaryLightColor,),
+                          const SizedBox(height: smallPadding,),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Category:', style: TextStyle(
+                                  color: Colors.white, fontSize: 16),),
+                              Text('${card['category']}',
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 16),),
+                            ],
+                          ),
+
+                          const SizedBox(height: smallPadding,),
+                          const Divider(color: kPrimaryLightColor,),
+                          const SizedBox(height: smallPadding,),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Price:', style: TextStyle(
+                                  color: Colors.white, fontSize: 16),),
+                              Text('${card['price']}',
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 16),),
+                            ],
+                          ),
+
+                          const SizedBox(height: smallPadding,),
+                          const Divider(color: kPrimaryLightColor,),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              const Expanded(child: Text("Action:",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16))),
+
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.remove_red_eye, color: Colors.white,),
+                                onPressed: () {
+                                  // .................
+
+                                },
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.edit, color: Colors.white,),
+                                onPressed: () {
+                                  // ..............................
+
+                                },
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.delete, color: Colors.white,),
+                                onPressed: () {
+                                  _showDeleteClientDialog();
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _showDeleteClientDialog() async {
+    return (await showDialog(
+      context: context,
+      builder: (context) =>
+          AlertDialog(
+            title: const Text("Delete Product"),
+            content: const Text("Do you really want to delete this product?"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false), // No
+                child: const Text("No"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true); // Yes
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Product deleted successfully!"),
+                    ),
+                  );
+                },
+                child: const Text("Yes"),
+              ),
+            ],
+          ),
+    )) ?? false;
   }
 
 }
