3e1fcfa4d15a32e5f5d9af70f77da36e33bb0a0d
i15tech <i15tech.global@gmail.com>
2024-10-29 10:29:03 +0530

    Quickcash
    -Invoices Screens
      - Invoice Dashboard Screen Done
         - Quotes Dashboard Screen Design Done
         - Add Invoice Screen Design Done
         - Add Quote Screen Design Done
      - Client(Invoice) Screen Design Done
         - Add Clients Screen Design Done
         - Edit Clients Screen Design Done
         - View Clients Screen Design Done
         - Client Home Screen Design Done
      - Categories(Invoice) Screen Design Done
         - Search
         - Add Categories
         - Show Categories
         - Edit Categories
         - Delete Categories

--- lib/Screens/InvoicesScreen/CategoriesScreen/categories_screen.dart
+++ lib/Screens/InvoicesScreen/CategoriesScreen/categories_screen.dart
@@ -9,7 +9,6 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
-
   final List<Map<String, String>> categoriesList = [
     {
       'createdDate': '2024-10-20',
@@ -30,27 +29,25 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
         iconTheme: const IconThemeData(color: Colors.white),
-        title: const
-        Text("Categories",
-          style: TextStyle(color: Colors.white),),
+        title: const Text(
+          "Categories",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: SingleChildScrollView(
-        child: Padding(padding: const EdgeInsets.all(defaultPadding),
+        child: Padding(
+          padding: const EdgeInsets.all(defaultPadding),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-
                   Expanded(
                     child: TextFormField(
                       keyboardType: TextInputType.text,
                       textInputAction: TextInputAction.next,
                       cursorColor: kPrimaryColor,
-                      onSaved: (value){
-                      },
-
                       decoration: InputDecoration(
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(defaultPadding),
@@ -67,19 +64,17 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                       ),
                     ),
                   ),
-
-                  const SizedBox(width: defaultPadding,),
-
+                  const SizedBox(width: defaultPadding),
                   FloatingActionButton(
                     onPressed: () {
-
+                      // Add your onPressed code here
+                      _addCategoryDialog();
                     },
-                    child: const Icon(Icons.add,color: kPrimaryColor,),
+                    child: const Icon(Icons.add, color: kPrimaryColor),
                   ),
                 ],
               ),
-
-              const SizedBox(height: largePadding,),
+              const SizedBox(height: largePadding),
               ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
@@ -87,7 +82,7 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                 itemBuilder: (context, index) {
                   final card = categoriesList[index];
                   return Padding(
-                    padding: const EdgeInsets.only(bottom: defaultPadding), // Add spacing
+                    padding: const EdgeInsets.only(bottom: defaultPadding),
                     child: Container(
                       width: double.infinity,
                       padding: const EdgeInsets.all(defaultPadding),
@@ -109,53 +104,45 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text('Created Date:', style: TextStyle(color: Colors.white, fontSize: 16),),
-                              Text('${card['createdDate']}', style: const TextStyle(color: Colors.white, fontSize: 16),),
+                              const Text('Created Date:', style: TextStyle(color: Colors.white, fontSize: 16)),
+                              Text('${card['createdDate']}', style: const TextStyle(color: Colors.white, fontSize: 16)),
                             ],
                           ),
-
-                          const SizedBox(height: smallPadding,),
-                          const Divider(color: kPrimaryLightColor,),
-                          const SizedBox(height: smallPadding,),
-
+                          const SizedBox(height: smallPadding),
+                          const Divider(color: kPrimaryLightColor),
+                          const SizedBox(height: smallPadding),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text('Category:',style: TextStyle(color: Colors.white, fontSize: 16),),
-                              Text('${card['category']}',style: const TextStyle(color: Colors.white, fontSize: 16),),
+                              const Text('Category:', style: TextStyle(color: Colors.white, fontSize: 16)),
+                              Text('${card['category']}', style: const TextStyle(color: Colors.white, fontSize: 16)),
                             ],
                           ),
-
-                          const SizedBox(height: smallPadding,),
-                          const Divider(color: kPrimaryLightColor,),
-                          const SizedBox(height: smallPadding,),
-
-
+                          const SizedBox(height: smallPadding),
+                          const Divider(color: kPrimaryLightColor),
+                          const SizedBox(height: smallPadding),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text('Product:',style: TextStyle(color: Colors.white, fontSize: 16),),
-                              Text('${card['product']}',style: const TextStyle(color: Colors.white, fontSize: 16),),
+                              const Text('Product:', style: TextStyle(color: Colors.white, fontSize: 16)),
+                              Text('${card['product']}', style: const TextStyle(color: Colors.white, fontSize: 16)),
                             ],
                           ),
-
-                          const SizedBox(height: smallPadding,),
-                          const Divider(color: kPrimaryLightColor,),
-                          const SizedBox(height: smallPadding,),
-
+                          const SizedBox(height: smallPadding),
+                          const Divider(color: kPrimaryLightColor),
+                          const SizedBox(height: smallPadding),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              const Expanded(child: Text("Action:",style: TextStyle(color: Colors.white, fontSize: 16))),
+                              const Expanded(child: Text("Action:", style: TextStyle(color: Colors.white, fontSize: 16))),
                               IconButton(
-                                icon: const Icon(Icons.edit, color: Colors.white,),
+                                icon: const Icon(Icons.edit, color: Colors.white),
                                 onPressed: () {
-
-
+                                  _editCategoryDialog(card);
                                 },
                               ),
                               IconButton(
-                                icon: const Icon(Icons.delete, color: Colors.white,),
+                                icon: const Icon(Icons.delete, color: Colors.white),
                                 onPressed: () {
                                   _showDeleteClientDialog();
                                 },
@@ -168,13 +155,150 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                   );
                 },
               ),
-
             ],
           ),
         ),
       ),
     );
   }
+
+  Future<void> _editCategoryDialog(Map<String, String> category) async {
+    final TextEditingController categoryController = TextEditingController(text: category['category']);
+
+    // Form key for validation
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit Category'),
+          content: Form( // Wrap in Form to enable validation
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: categoryController, // Set controller to TextFormField
+                  keyboardType: TextInputType.text,
+                  cursorColor: kPrimaryColor,
+                  textInputAction: TextInputAction.none,
+                  maxLines: 4,
+                  minLines: 1,
+                  decoration: InputDecoration(
+                    labelText: 'Category',
+                    labelStyle: const TextStyle(color: kPrimaryColor),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Category is required';
+                    }
+                    return null; // Validation passed
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) { // Validate the form
+                  setState(() {
+                    category['category'] = categoryController.text;
+                  });
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Category updated successfully!')),
+                  );
+                }
+              },
+              child: const Text('Save'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  Future<void> _addCategoryDialog() async {
+
+    // Form key for validation
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Category'),
+          content: Form( // Wrap in Form to enable validation
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  cursorColor: kPrimaryColor,
+                  textInputAction: TextInputAction.none,
+                  maxLines: 4,
+                  minLines: 1,
+                  decoration: InputDecoration(
+                    labelText: 'Category',
+                    labelStyle: const TextStyle(color: kPrimaryColor),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Category is required';
+                    }
+                    return null; // Validation passed
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) { // Validate the form
+
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Category added successfully!')),
+                  );
+                }
+              },
+              child: const Text('Save'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 
   Future<bool> _showDeleteClientDialog() async {
     return (await showDialog(
@@ -202,6 +326,4 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
       ),
     )) ?? false;
   }
-
-
 }
