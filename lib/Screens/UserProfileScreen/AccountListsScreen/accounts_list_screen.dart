05bcdf4725b32d766aec7abd900a4546c9e1d0d7
i15tech <i15tech.global@gmail.com>
2024-11-06 17:06:52 +0530

    Quickcash
    1. Login Api - Done
         - Api Constant Class (Base URL)
         - Login Api Class
         - Login Model Class
         - Save UserId
         - Save Token
         - Save User Email
         - Save User Name
    
    2. Auth Manager Class (Save userId and Access Token through SharedPreference) - Done
         - Save UserID
         - Save Access Token
         - Save User Name
         - Save User Email
         - Get UserId
         - Get Access Token
         - Get User Name
         - Get User Email
         ---------  When Click on Logout Button -----------
         - Clear Access Token - Logout
         - Clear UserId - Logout
    
    3. Welcome Screen - Done
       - IF User is already login show Home Screen
       - If User is not login show WelCome Screen UI
    
    4. SignUp Screen UI
        - Add Country Fields
    
    5. SignUp Api - Done
             - SignUp Api Class
             - SignUp Model Class
             - Save UserId
             - Save Token
             - Save User Name
             - Save User Email
    
    6. In Drawer Header - Done
         - Show User Name
         - Show User Image
         - Show User Email
    
    7. Forgot Password Api - Done
         - Forgot Password Api class
         - Forgot Password model class
    
    8. User Profile Api - Done
         - User Profile Api Class with auth
         - User Profile Model class
         - Fetch All Data
         - Set All Data
         - Show User Image
    
    9. Documents (User) Api - Done
         - Documents Api Class
         - Documents Model Class
         - Fetch All Data
         - Set All Data
    
    10. Accounts List Screen (User) -
         - UI Change
         - Accounts List Api Class
         - Accounts Model Class
         - Fetch All Data
         - Set All Accounts List Data

--- lib/Screens/UserProfileScreen/AccountListsScreen/accounts_list_screen.dart
+++ lib/Screens/UserProfileScreen/AccountListsScreen/accounts_list_screen.dart
@@ -3,6 +3,9 @@ import 'package:quickcash/Screens/UserProfileScreen/AccountListsScreen/model/acc
 import 'package:quickcash/constants.dart';
 import 'package:quickcash/util/auth_manager.dart';
 
+// Make sure to import your model
+import 'model/accountsListModel.dart';
+
 class AccountsListScreen extends StatefulWidget {
   const AccountsListScreen({super.key});
 
@@ -11,14 +14,14 @@ class AccountsListScreen extends StatefulWidget {
 }
 
 class _AccountsListScreen extends State<AccountsListScreen> {
-
   final AccountsListApi _accountsListApi = AccountsListApi();
 
   bool isLoading = false;
   String? errorMessage;
+  List<AccountDetail> accountData = []; // List to hold AccountDetail objects
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     mAccountsDetails();
   }
@@ -29,49 +32,28 @@ class _AccountsListScreen extends State<AccountsListScreen> {
       errorMessage = null;
     });
 
-    try{
-
+    try {
       final response = await _accountsListApi.accountListApi();
 
-      print(response.accountDetails);
-
-
-      setState(() {
-        isLoading = false;
-      });
-    }catch (error) {
+      // Check if the response has account details and update the state
+      if (response.accountDetails != null && response.accountDetails!.isNotEmpty) {
+        setState(() {
+          accountData = response.accountDetails!; // Update accountData with the API response
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+          errorMessage = 'No accounts found.';
+        });
+      }
+    } catch (error) {
       setState(() {
         isLoading = false;
         errorMessage = error.toString();
       });
     }
-
   }
-// This is Dummy list
-  final List<Map<String, String>> accountData = [
-    {
-      "currency": "USD",
-      "iban": "US123456789",
-      "bic": "USBANK1",
-      "balance": "\$1500.00",
-      "image": 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmN0el3AEK0rjTxhTGTBJ05JGJ7rc4_GSW6Q&s', // Add image URL
-    },
-    {
-      "currency": "EUR",
-      "iban": "EU987654321",
-      "bic": "EUBANK2",
-      "balance": "€2000.00",
-      "image": 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmN0el3AEK0rjTxhTGTBJ05JGJ7rc4_GSW6Q&s', // Add image URL
-    },
-    {
-      "currency": "GBP",
-      "iban": "GB112233445",
-      "bic": "GBBANK3",
-      "balance": "£1000.00",
-      "image": 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmN0el3AEK0rjTxhTGTBJ05JGJ7rc4_GSW6Q&s', // Add image URL
-    },
-    // Add more accounts as needed
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -81,133 +63,136 @@ class _AccountsListScreen extends State<AccountsListScreen> {
         child: Column(
           children: [
             const SizedBox(height: largePadding),
-            // Horizontal Scroll List of Account Data with Images
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,  // Makes the ListView only take as much space as it needs
-                scrollDirection: Axis.horizontal,
-                itemCount: accountData.length,
-                itemBuilder: (context, index) {
-                  var account = accountData[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Card(
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(defaultPadding),
-                      ),
-                      child: Container(
-                        width: 320,
-                        padding: const EdgeInsets.all(defaultPadding),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
+            // Check if data is loading or there's an error
+            if (isLoading)
+              const Center(child: CircularProgressIndicator()),
+            if (errorMessage != null)
+              Center(child: Text(errorMessage!, style: TextStyle(color: Colors.red))),
 
-                            const SizedBox(height: largePadding),
-                            Center(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(100),
-                                child: Image.network(
-                                  account['image']!,
-                                  width: 200,
-                                  height: 200,
-                                  fit: BoxFit.cover,
+            // Display account data when available
+            if (!isLoading && errorMessage == null && accountData.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: accountData.length,
+                  itemBuilder: (context, index) {
+                    var account = accountData[index];
+
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(defaultPadding),
+                        ),
+                        child: Container(
+                          width: 320,
+                          padding: const EdgeInsets.all(defaultPadding),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: largePadding),
+
+                              // Image (Add default or custom image based on your model)
+                              Center(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: Image.network(
+                                    'https://media.istockphoto.com/id/1471401435/vector/round-icon-of-indian-flag.jpg?s=612x612&w=0&k=20&c=kXy7vTsyhEycfrQ9VmI4FpfBFX2cMtQP5XIvTdU8xDE=', // Placeholder image
+                                    width: 200,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
 
-                            const SizedBox(height: 35),
-                            const Divider(color: kWhiteColor),
-                            const SizedBox(height: defaultPadding),
+                              const SizedBox(height: 35),
+                              const Divider(color: kWhiteColor),
+                              const SizedBox(height: defaultPadding),
 
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Currency: ${account['currency']}',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Currency: ${account.currency ?? 'N/A'}',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
+                                  const SizedBox(height: smallPadding),
+                                  const Divider(color: kWhiteColor),
+                                  const SizedBox(height: smallPadding),
 
-                                const SizedBox(height: smallPadding),
-                                const Divider(color: kWhiteColor),
-                                const SizedBox(height: smallPadding),
-
-                                const Text(
-                                  'IBAN / Routing / Account Number:',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                                  const Text(
+                                    'IBAN / Routing / Account Number:',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
-
-                                Text(
-                                  '${account['iban']}',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                                  Text(
+                                    account.iban ?? 'N/A',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
+                                  const SizedBox(height: smallPadding),
+                                  const Divider(color: kWhiteColor),
+                                  const SizedBox(height: smallPadding),
 
-                                const SizedBox(height: smallPadding),
-                                const Divider(color: kWhiteColor),
-                                const SizedBox(height: smallPadding),
-
-                                const Text(
-                                  'BIC / IFSC:',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                                  const Text(
+                                    'BIC / IFSC:',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
-
-                                Text(
-                                  '${account['bic']}',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                                  Text(
+                                    account.bicCode?.toString() ?? 'N/A',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
+                                  const SizedBox(height: smallPadding),
+                                  const Divider(color: kWhiteColor),
+                                  const SizedBox(height: smallPadding),
 
-                                const SizedBox(height: smallPadding),
-                                const Divider(color: kWhiteColor),
-                                const SizedBox(height: smallPadding),
-
-                                const Text(
-                                  'Balance:',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                                  const Text(
+                                    'Balance:',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
-
-                                Text(
-                                  '${account['balance']}',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: kPrimaryColor,
+                                  Text(
+                                    '${account.amount ?? 0.0}', // Directly displaying the amount without rounding
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimaryColor,
+                                    ),
                                   ),
-                                ),
 
-                              ],
-                            ),
-                          ],
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
             const SizedBox(height: 30),
           ],
         ),
