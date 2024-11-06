dcb62dae5b95798993391856745e5d394698e7bb
i15tech <i15tech.global@gmail.com>
2024-11-06 12:55:49 +0530

    Quickcash
    1. Login Api Done
         - Api Constant Class (Base URL)
         - Login Api Class
         - Login Model Class
         - Save UserId
         - Save Token
         - Save User Email
         - Save User Name
    
    2. Auth Manager Class (Save userId and Access Token through SharedPreference)
         - Save UserID
         - Save Access Token
         - Save User Name
         - Save User Email
         - Get UserId
         - Get Access Token
         - Get User Name
         - Get User Email
         ---------  When Click on Logout Button -----------
         - Clear Access Token - Logout
         - Clear UserId - Logout
    
    3. Welcome Screen
       - IF User is already login show Home Screen
       - If User is not login show WelCome Screen UI
    
    4. SignUp Screen UI
        - Add Country Fields
    
    5. SignUp Api Done
             - SignUp Api Class
             - SignUp Model Class
             - Save UserId
             - Save Token
             - Save User Name
             - Save User Email
    
    6. In Drawer Header
         - Show User Name
         - Show User Image
         - Show User Email
    
    7. Forgot Password Api Done
         - Forgot Password Api class
         - Forgot Password model class
    
    8. User Profile Api
         - User Profile Api Class with auth
         - User Profile Model class
         - Fetch All Data
         - Set All Data
         - Show User Image

--- lib/Screens/UserProfileScreen/UserProfileScreen/userInformation_screen.dart
+++ lib/Screens/UserProfileScreen/UserProfileScreen/userInformation_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quickcash/Screens/UserProfileScreen/UserProfileScreen/model/userProfileApi.dart';
+import 'package:quickcash/util/apiConstants.dart';
+import 'package:quickcash/util/auth_manager.dart';
 import '../../../../constants.dart';
 
 class UserInformationScreen extends StatefulWidget {
@@ -17,12 +19,15 @@ class _UserInformationScreenState extends State<UserInformationScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _mobileController = TextEditingController();
   final TextEditingController _countryController = TextEditingController();
-  final TextEditingController _totalAccountsController = TextEditingController();
-  final TextEditingController _defaultCurrencyController = TextEditingController();
+  final TextEditingController _totalAccountsController =
+      TextEditingController();
+  final TextEditingController _defaultCurrencyController =
+      TextEditingController();
   final TextEditingController _addressController = TextEditingController();
 
   bool isLoading = false;
   String? errorMessage;
+  String? profileImageUrl;
 
   @override
   void initState() {
@@ -39,29 +44,34 @@ class _UserInformationScreenState extends State<UserInformationScreen> {
     try {
       final response = await _userProfileApi.userProfile();
 
-      print(response.ownerProfile);
 
+      // Set the profile image URL dynamically
+      if (response.ownerProfile != null) {
+        // Assuming response.ownerProfile contains the image filename
+        profileImageUrl =
+            '${ApiConstants.baseImageUrl}${AuthManager.getUserId()}/${response.ownerProfile}';
+      }
 
-      if(response.name !=null) {
+      if (response.name != null) {
         _fullNameController.text = response.name!;
       }
-      if(response.email !=null){
+      if (response.email != null) {
         _emailController.text = response.email!;
       }
-      if(response.mobile !=null){
+      if (response.mobile != null) {
         _mobileController.text = response.mobile!;
       }
-      if(response.country !=null){
+      if (response.country != null) {
         _countryController.text = response.country!;
       }
-      if(response.defaultCurrency !=null){
+      if (response.defaultCurrency != null) {
         _defaultCurrencyController.text = response.defaultCurrency.toString();
       }
-      if (response.address !=null) {
+      if (response.address != null) {
         _addressController.text = response.address!;
       }
-      _totalAccountsController.text = response.accountDetails?.length.toString() ?? '0';
-
+      _totalAccountsController.text =
+          response.accountDetails?.length.toString() ?? '0';
 
       setState(() {
         isLoading = false;
@@ -80,172 +90,195 @@ class _UserInformationScreenState extends State<UserInformationScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(padding: const EdgeInsets.all(defaultPadding),
+            Padding(
+              padding: const EdgeInsets.all(defaultPadding),
               child: Column(
                 children: [
-                  const Center(
-                    child: Stack(
-                      alignment: Alignment.bottomRight,
-                      children: [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundImage: AssetImage('assets/images/profile_pic.png'), // Replace with actual image asset
-                        ),
-                      ],
+                  const SizedBox(height: defaultPadding),
+
+                  // Profile Image Section
+                  if (profileImageUrl != null)
+                    Center(
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(profileImageUrl!),
+                          ),
+                        ],
+                      ),
+                    )
+                  else if (isLoading)
+                    const Center(
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage(
+                                'assets/images/profile_pic.png'), // Replace with actual image asset
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: defaultPadding),
-
-                  const Text(
-                    'User Name',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: kPrimaryColor),
-                  ),
+                  // Show loading indicator
 
                   const SizedBox(height: defaultPadding),
 
-                  if (isLoading) const CircularProgressIndicator(color: kPrimaryColor,), // Show loading indicator
-                  if (errorMessage != null) // Show error message if there's an error
-                    Text(errorMessage!, style: const TextStyle(color: Colors.red)),
-                  const SizedBox(height: defaultPadding,),
+                  Text(
+                    _fullNameController.text,
+                    style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: kPrimaryColor),
+                  ),
 
+                  const SizedBox(height: defaultPadding),
+
+                  if (isLoading)
+                    const CircularProgressIndicator(
+                      color: kPrimaryColor,
+                    ),
+                  // Show loading indicator
+                  if (errorMessage !=
+                      null) // Show error message if there's an error
+                    Text(errorMessage!,
+                        style: const TextStyle(color: Colors.red)),
+                  const SizedBox(
+                    height: defaultPadding,
+                  ),
 
                   // Update the text fields with TextEditingController
                   TextFormField(
-                    controller: _fullNameController, // Bind the controller
+                    controller: _fullNameController,
+                    // Bind the controller
                     keyboardType: TextInputType.text,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Full Name",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
                   const SizedBox(height: defaultPadding),
                   TextFormField(
-                    controller: _emailController, // Bind the controller
+                    controller: _emailController,
+                    // Bind the controller
                     keyboardType: TextInputType.emailAddress,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Your Email",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
                   const SizedBox(height: defaultPadding),
                   TextFormField(
-                    controller: _mobileController, // Bind the controller
+                    controller: _mobileController,
+                    // Bind the controller
                     keyboardType: TextInputType.phone,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Mobile Number",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
                   const SizedBox(height: defaultPadding),
                   TextFormField(
-                    controller: _countryController, // Bind the controller
+                    controller: _countryController,
+                    // Bind the controller
                     keyboardType: TextInputType.text,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Country",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
                   const SizedBox(height: defaultPadding),
                   TextFormField(
-                    controller: _totalAccountsController, // Bind the controller
+                    controller: _totalAccountsController,
+                    // Bind the controller
                     keyboardType: TextInputType.text,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Total Accounts",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
                   const SizedBox(height: defaultPadding),
                   TextFormField(
-                    controller: _defaultCurrencyController, // Bind the controller
+                    controller: _defaultCurrencyController,
+                    // Bind the controller
                     keyboardType: TextInputType.text,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Default Currency",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
                   const SizedBox(height: defaultPadding),
                   TextFormField(
-                    controller: _addressController, // Bind the controller
+                    controller: _addressController,
+                    // Bind the controller
                     keyboardType: TextInputType.text,
                     textInputAction: TextInputAction.next,
                     cursorColor: kPrimaryColor,
                     onSaved: (value) {},
                     readOnly: true,
                     style: const TextStyle(color: kPrimaryColor),
-
                     decoration: InputDecoration(
                       labelText: "Address",
                       labelStyle: const TextStyle(color: kPrimaryColor),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide()
-                      ),
+                          borderSide: const BorderSide()),
                     ),
                   ),
 
@@ -259,4 +292,3 @@ class _UserInformationScreenState extends State<UserInformationScreen> {
     );
   }
 }
-
--- lib/util/apiConstants.dart
+++ lib/util/apiConstants.dart
@@ -1,3 +1,4 @@
 class ApiConstants {
   static const String baseUrl = 'https://quickcash.oyefin.com/api/v1';
+  static const String baseImageUrl = 'https://quickcash.oyefin.com/storage/profile/';
 }
