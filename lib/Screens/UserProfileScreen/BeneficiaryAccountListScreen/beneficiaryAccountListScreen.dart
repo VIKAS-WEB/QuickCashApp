ab0f021f4d51144949ff21f9440975582464862f
i15tech <i15tech.global@gmail.com>
2024-11-07 11:52:46 +0530

    Quickcash
    1. Welcome Screen - Done
       - IF User is already login show Home Screen
       - If User is not login show WelCome Screen UI
    
    2. SignUp Screen UI
        - Add Country Fields
    
    3. SignUp Api - Done
             - SignUp Api Class
             - SignUp Model Class
             - Save UserId
             - Save Token
             - Save User Name
             - Save User Email
    
    4. In Drawer Header - Done
         - Show User Name
         - Show User Image
         - Show User Email
    
    5. Forgot Password Api - Done
         - Forgot Password Api class
         - Forgot Password model class
    
    6. User Profile Api - Done
         - User Profile Api Class with auth
         - User Profile Model class
         - Fetch All Data
         - Set All Data
         - Show User Image
    
    7. Documents (User) Api - Done
         - Documents Api Class
         - Documents Model Class
         - Fetch All Data
         - Set All Data
    
    8. Accounts List Screen (User) Api Done
         - UI Change
         - Accounts List Api Class
         - Accounts Model Class
         - Fetch All Data
         - Set All Accounts List Data
    
    9. Additional Information Screen (User) Api Done
         - Additional Information Api class
         - Additional Information Model Class
         - Fetch All Data
         - Show All Data
    
    10. BeneficiaryAccountList Screen (User) Design and Api - Done
         - UI Design - Done
         -

--- lib/Screens/UserProfileScreen/BeneficiaryAccountListScreen/beneficiaryAccountListScreen.dart
+++ lib/Screens/UserProfileScreen/BeneficiaryAccountListScreen/beneficiaryAccountListScreen.dart
@@ -12,86 +12,135 @@ class BeneficiaryAccountListScreen extends StatefulWidget {
 class _BeneficiaryAccountListState extends State<BeneficiaryAccountListScreen> {
   final List<Map<String, String>> recentTrades = [
     {
-      "price": "71417.67000000",
-      "quantity": "0.00018000",
-      "time": "05:33:28 PM"
+      "currency": "USD",
+      "iban": "LT955424154545454",
+      "bic": "321456",
+      "balance": "\$0.00018000"
     },
     {
-      "price": "71417.67000000",
-      "quantity": "0.00018000",
-      "time": "05:33:38 PM"
+      "currency": "USD",
+      "iban": "LT955424154545454",
+      "bic": "321456",
+      "balance": "\$0.00018000"
     },
     {
-      "price": "71417.67000000",
-      "quantity": "0.00018000",
-      "time": "05:33:38 PM"
+      "currency": "USD",
+      "iban": "LT955424154545454",
+      "bic": "321456",
+      "balance": "\$0.00018000"
     },
     {
-      "price": "71417.67000000",
-      "quantity": "0.00018000",
-      "time": "05:33:38 PM"
+      "currency": "USD",
+      "iban": "LT955424154545454",
+      "bic": "321456",
+      "balance": "\$0.00018000"
     },
-    {
-      "price": "71417.67000000",
-      "quantity": "0.00018000",
-      "time": "05:33:38 PM"
+    {"currency": "USD",
+      "iban": "LT955424154545454",
+      "bic": "321456",
+      "balance": "\$0.00018000"
     },
+
     // Add more trades here if needed
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Simple list using Column without scroll
-          Expanded(
-            child: ListView.builder(
-              itemCount: recentTrades.length,
-              itemBuilder: (context, index) {
-                final trade = recentTrades[index];
-                return Container(
-                  margin: const EdgeInsets.only(bottom: smallPadding),
-                  padding: const EdgeInsets.all(defaultPadding),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: kPurpleColor, width: 1),
+      body: Padding(
+        padding: const EdgeInsets.all(defaultPadding),  // This will add padding to the entire screen body
+        child: ListView.builder(
+          itemCount: recentTrades.length,
+          itemBuilder: (context, index) {
+            final trade = recentTrades[index];
+            return Container(
+              margin: const EdgeInsets.only(bottom: defaultPadding),  // Margin for card spacing
+              decoration: BoxDecoration(
+                color: kWhiteColor,  // Background color of the container
+                borderRadius: BorderRadius.circular(smallPadding),  // Rounded corners for the container
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 8,
+                    spreadRadius: 1,
+                    offset: const Offset(0, 4),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text("Price (USDT):", style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold)),
-                          Text("${trade['price']}", style: const TextStyle(color: kPrimaryColor)),
-                        ],
+                ],
+                border: Border.all(
+                  color: kPurpleColor,  // Purple border color
+                  width: 1,  // Border width
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(defaultPadding),  // Padding for content inside the container
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    // Display currency code inside a circle
+                    const Center(
+                      child: CircleAvatar(
+                        radius: 50, // Size of the circle
+                        backgroundColor: kPrimaryColor, // Background color of the circle
+                        child: Text(
+                          'USD', // Display the currency code
+                          style: TextStyle(
+                            fontSize: 30, // Font size of the currency code
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // Text color
+                          ),
+                        ),
                       ),
-                      const SizedBox(height: smallPadding),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text("Qty (BTC):", style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold)),
-                          Text("${trade['quantity']}", style: const TextStyle(color: kPrimaryColor)),
-                        ],
-                      ),
-                      const SizedBox(height: smallPadding),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text("Time:", style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold)),
-                          Text("${trade['time']}", style: const TextStyle(color: kPrimaryColor)),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+                    ),
+
+                    const SizedBox(height: largePadding,),
+
+                    const Text(
+                      "Currency:",
+                      style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${trade['currency']}",
+                      style: const TextStyle(color: kPrimaryColor),
+                    ),
+
+
+                    const SizedBox(height: 8),  // Small space between rows
+                    const Text(
+                      "IBAN / Routing / Account Number",
+                      style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${trade['iban']}",
+                      style: const TextStyle(color: kPrimaryColor),
+                    ),
+
+                    const SizedBox(height: smallPadding,),
+
+                    const Text(
+                      "BIC / IFSC:",
+                      style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${trade['bic']}",
+                      style: const TextStyle(color: kPrimaryColor),
+                    ),
+
+                    const SizedBox(height: 8),  // Small space between rows
+                    const Text(
+                      "Balance:",
+                      style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${trade['balance']}",
+                      style: const TextStyle(color: kPrimaryColor),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
