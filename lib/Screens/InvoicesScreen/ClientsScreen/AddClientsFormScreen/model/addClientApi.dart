330ecfcbcec4fc9369d75062c14fc03aedb95354
i15tech <i15tech.global@gmail.com>
2024-11-19 13:13:36 +0530

    Quickcash
    1.  Wallet Address Api -  Done
        - Wallet Address Api Class
        - Wallet Address Model class
        - Fetch data
        - Show data
    
    2. Add New Wallet Address api - Done
        - Add New Wallet Design - BottomSheet Show
        - Add New Wallet Api Class
        - Add New Wallet Model Class
        - Api Integrated
    
    3. Crypto Transaction List Api - Done
        - Crypto Transaction List Api class
        - Crypto Transaction List Model class
        - Fetch Data
        - Show all data
    
    1. Clients List Api - Done
        - Clients List Api class
        - Clients Model class
        - fetch data
        - show data
        - send clients Id - View clients Screen
    
    2. View Clients Api - Done
       - View Clients api class
       - View Clients Model
       - fetch data
       - show data
    
    3. Update Client Api - Done
       - Update Client api class
       - Update client model class
       - api integration
    
    4. Add Client Api - done
       - Add Client Api class
       - Add client Model class
       - api integration

---
+++ lib/Screens/InvoicesScreen/ClientsScreen/AddClientsFormScreen/model/addClientApi.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:quickcash/util/auth_manager.dart';
+import '../../../../../util/apiConstants.dart';
+import 'addClientModel.dart';
+
+class AddClientApi {
+  final Dio _dio = Dio();
+
+  AddClientApi() {
+    _dio.options.baseUrl = ApiConstants.baseUrl;
+    _dio.options.headers['Authorization'] = 'Bearer ${AuthManager.getToken()}';
+
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+    ));
+  }
+
+
+  Future<AddClientResponse> addClient(AddClientRequest request) async {
+    try {
+      final response = await _dio.post(
+        '/client/add',
+        data: request.toJson(),
+      );
+
+      // Check for a successful response
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return AddClientResponse.fromJson(response.data);
+      } else {
+        throw Exception('Failed to update profile: ${response.statusMessage}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Dio error: ${e.response?.data ?? e.message}');
+    } catch (e) {
+      throw Exception('Unexpected error: $e');
+    }
+  }
+}
