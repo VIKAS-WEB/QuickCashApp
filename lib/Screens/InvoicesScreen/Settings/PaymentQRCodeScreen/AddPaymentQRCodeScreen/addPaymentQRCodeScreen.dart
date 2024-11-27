9a8765cd2c2cd476e1f58485b4fe62381c0271b5
i15tech <i15tech.global@gmail.com>
2024-11-27 14:36:53 +0530

    Quickcash
    // Invoice - Quote
    1. Quotes List Api - Done
        - Quote List Api class
        - Quote List Model class
        - fetch data
        - show data
    
    2. Quote Reminder Api - Done
        - Quote Reminder Api class
        - Api integration
    
    3. Quote Delete Api -  Done
        - Quote Delete Api class
        - Api Integration
    
    4. Add Quote Api - ... Hold
         - Clients Api Integration for get Client Lists
         - Currency Api Integration for get Currency Lists
         - Product Api Integration for get Products List ...
    
    5. Tax Api - Done
        - Tax Api class
        - Tax Api model class
        - Api Integration
    
    // Invoices Settings -
    1. Invoice Settings details Api- Done
        - Settings Details Api class
        - Settings Details Model class
        - Fetch data
        - show data
    
    2. Add Tax Screen Design And Validation - Done
    3. Add Payment QR Code Design and Validation - Done

--- lib/Screens/InvoicesScreen/Settings/PaymentQRCodeScreen/AddPaymentQRCodeScreen/addPaymentQRCodeScreen.dart
+++ lib/Screens/InvoicesScreen/Settings/PaymentQRCodeScreen/AddPaymentQRCodeScreen/addPaymentQRCodeScreen.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:quickcash/constants.dart';
+
+import '../../../../../util/customSnackBar.dart';
 
 class AddPaymentQRCodeScreen extends StatefulWidget{
   const AddPaymentQRCodeScreen({super.key});
@@ -8,12 +14,186 @@ class AddPaymentQRCodeScreen extends StatefulWidget{
 }
 
 class _AddPaymentQRCodeScreenState extends State<AddPaymentQRCodeScreen>{
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController title = TextEditingController();
+  final TextEditingController taxRate = TextEditingController();
+  String? selectedType = "yes";
+  String? imagePath;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          "Add QR Code",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(defaultPadding),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  TextFormField(
+                    controller: title,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    cursorColor: kPrimaryColor,
+                    onSaved: (value) {},
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter title';
+                      }
+                      return null;
+                    },
+                    readOnly: false,
+                    style: const TextStyle(color: kPrimaryColor),
+                    decoration: InputDecoration(
+                      labelText: "Title",
+                      labelStyle:
+                      const TextStyle(color: kPrimaryColor, fontSize: 16),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(),
+                      ),
+                      filled: true,
+                      fillColor: Colors.transparent,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: largePadding,
+                  ),
 
+                  const Padding(padding: EdgeInsets.only(left: smallPadding),
+                  child: Text("Upload Payment QR-code",style: TextStyle(color: kPrimaryColor, fontWeight: FontWeight.bold),),),
+
+                  const SizedBox(height: 2.0,),
+                  Card(
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: imagePath != null
+                              ? Image.file(
+                            File(imagePath!),
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 250,
+                          )
+                              : Image.network(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmN0el3AEK0rjTxhTGTBJ05JGJ7rc4_GSW6Q&s',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 250,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 8,
+                          right: 8,
+                          child: GestureDetector(
+                            onTap: () async {
+                              final ImagePicker picker = ImagePicker();
+                              final XFile? image = await picker.pickImage(
+                                  source: ImageSource.gallery);
+
+                              if (image != null) {
+                                setState(() {
+                                  imagePath =
+                                      image.path; // Store the image path
+                                });
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Image selected')),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('No image selected.')),
+                                );
+                              }
+                            },
+                            child: const CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Icon(
+                                Icons.edit,
+                                color: kPrimaryColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+
+                  const SizedBox(height: largePadding),
+                  const Text(
+                    "Default",
+                    style: TextStyle(
+                        color: kPrimaryColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Radio<String>(
+                        value: 'yes',
+                        groupValue: selectedType,
+                        onChanged: (String? value) {
+                          setState(() {
+                            selectedType = value;
+                          });
+                        },
+                      ),
+                      const Text('Yes', style: TextStyle(color: kPrimaryColor)),
+                      Radio<String>(
+                        value: 'no',
+                        groupValue: selectedType,
+                        onChanged: (String? value) {
+                          setState(() {
+                            selectedType = value;
+                          });
+                        },
+                      ),
+                      const Text('No', style: TextStyle(color: kPrimaryColor)),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 45.0,
+                  ),
+                  Center(
+                    child: SizedBox(
+                      width: 180,
+                      height: 50.0,
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            CustomSnackBar.showSnackBar(context: context, message: "Submitted", color: kPrimaryColor);
+                          }
+                        },
+                        label: const Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                        backgroundColor: kPrimaryColor,
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ),
+      ),
     );
   }
-
 }
