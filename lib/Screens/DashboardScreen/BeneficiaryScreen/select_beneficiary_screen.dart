51309217edf4e03aaa3178b46d258193bb8dc9ad
i15tech <i15tech.global@gmail.com>
2024-12-03 14:18:45 +0530

    Quickcash
    // Exchange Money
    1. Pass Data from Dashboard Account to Exchange Screen - Done
          - accountIdExchange, countryExchange, currencyExchange, ibanExchange, statusExchange, amountExchange
    
    2. Show All Data Exchange Screen -  Done
    3. All Account Api Integrated
         - Show all account
         - select exchange money account
         - Validation
    
    4. Exchange Money Api - Done
        - Exchange Money Api class
        - Exchange Money model class
        - Api Integration
        - Fetch all data
        - Set All Data
        - Validation
    
    5. Pass Data Exchange Screen to Review Exchange Screen - Done
           ReviewExchangeMoneyScreen(
                                     fromAccountId: mFromAccountId,
                                     fromCountry:mFromCountry,
                                     fromCurrency: mFromCurrency,
                                     fromIban: mFromIban,
                                     fromAmount: mFromAmount,
                                     fromCurrencySymbol: mFromCurrencySymbol,
                                     fromTotalFees: mFromTotalFees,
                                     toAccountId: mToAccountId,
                                     toCountry:mToCountry,
                                     toCurrency: mToCurrency,
                                     toIban: mToIban,
                                     toAmount: mToAmount,
                                     toCurrencySymbol: mToCurrencySymbol)
    
    6. Show All Data On Review Exchange Screen - Done
    7. Add Exchange Transaction Api - done
         - Add Exchange Transaction Api class
         - Add Exchange Transaction Model class
         - Api Integration
    
    //  Send / Some OneNew - Api
    1.  Send Currency Api Integration - Done
          - Api Integration
          - Show Country Icon, Amount
    
    2. Receive Currency Api Integration - Done
          - Api Integration
          - Show Country Icon, Amount
    
    3. Exchange Money Api - Done
         - Api Integration
         - Fetch All Data
         - Show Data
         - Validation

--- lib/Screens/DashboardScreen/BeneficiaryScreen/select_beneficiary_screen.dart
+++ lib/Screens/DashboardScreen/BeneficiaryScreen/select_beneficiary_screen.dart
@@ -1,5 +1,4 @@
 import 'package:country_picker/country_picker.dart';
-import 'package:country_state_city_pro/country_state_city_pro.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
